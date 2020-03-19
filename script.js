40a7eb8b5d013fa7f88a7d64fbf051dded800bdc
Akos Somodi <akossomodi@yahoo.com>
2020-03-19 14:14:07 +0100

    create letter press event handler

--- script.js
+++ script.js
@@ -38,6 +38,36 @@ function displayWord() {
   }
 }
 
-function updateWrongLettersEl() {}
+// Update the wrong letters
+function updateWrongLettersElement() {}
+
+// Show notification
+function showNotification() {}
+
+// Keydown letter press
+window.addEventListener('keydown', e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersElement();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
