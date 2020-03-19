8dc5d2580704f08dbb8333d784f13f68aefe6e23
Akos Somodi <akossomodi@yahoo.com>
2020-03-19 12:22:55 +0100

    create display words function and win condition checking😃

--- script.js
+++ script.js
@@ -14,5 +14,30 @@ const words = ['application', 'programming', 'interface', 'wizard'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = [];
+const correctLetters = ['w', 'i', 'z', 'a', 'r', 'd'];
 const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordElement.innerHTML = `${selectedWord
+    .split('')
+    .map(
+      letter => `
+        <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+    `
+    )
+    .join('')}
+    `;
+  const innerWord = wordElement.innerText.replace(/\n/g, ''); // regex : globally replace the element with an empty string
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 😃';
+    popup.style.display = 'flex';
+  }
+}
+
+function updateWrongLettersEl() {}
+
+displayWord();
